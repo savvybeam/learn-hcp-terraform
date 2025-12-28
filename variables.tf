80d558237db19adc58896162ff737367c666bcb1
Oluwasegun Salami <124251724+savvybeam@users.noreply.github.com>
2025-12-28 18:10:44 +0000

    Comment out AWS access key variables
    Comment out AWS access key variables in Terraform. Terraform Plan was failing so I have tried to comment this out as ENV should be read from HCP.

--- variables.tf
+++ variables.tf
@@ -10,6 +10,6 @@ variable "instance_type" {
   default     = "t2.micro"
 }
 
-variable "AWS_ACCESS_KEY_ID" {}
+#variable "AWS_ACCESS_KEY_ID" {}
 
-variable "AWS_SECRET_ACCESS_KEY" {}
+#variable "AWS_SECRET_ACCESS_KEY" {}
